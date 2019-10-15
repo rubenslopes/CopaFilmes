d2971ed4ff7d1eb5074c344ed00b8fcec7cadf99
Rubens Armôa Lopes <rubens.lopes@gmail.com>
2019-10-15 07:00:45 -0400

    code cleanup

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -9,23 +9,20 @@ using Microsoft.Extensions.Logging;
 
 namespace copa_filmes.Pages
 {
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public class ErrorModel : PageModel
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public class ErrorModel : PageModel
+  {
+    public ErrorModel()
     {
-        private readonly ILogger<ErrorModel> _logger;
-
-        public ErrorModel(ILogger<ErrorModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
-        public void OnGet()
-        {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-        }
     }
+
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    public void OnGet()
+    {
+      RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+    }
+  }
 }
